4fe6c406ad1da587d13d671f91a84d963b168fee
Danny Hoang <bajanboss132@gmail.com>
2023-03-26 06:00:18 +0100

    made a GUI based on the form layout - it takes text input and outputs it when submitted.

--- Gui/form_demo.py
+++ Gui/form_demo.py
@@ -7,17 +7,31 @@ class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         #MainWindow Constructor
         super().__init__()
-        #Custom Code Start
+        ############################Custom Code Start
+        self.setGeometry(100,100,300,300)
+
         form_layout = QtWidgets.QFormLayout()
         self.setLayout(form_layout)
-        #setting the title 
-        form_layout.addRow(QtWidgets.QLabel("Really cool form"))
-        #setting first input
+
+        form_layout.addRow(QtWidgets.QLabel("<b>Really cool form</b>", self))
+
         self.line1 = QtWidgets.QLineEdit(self)
         form_layout.addRow("Name: " , self.line1)
 
+        self.button : QtWidgets.QPushButton = QtWidgets.QPushButton("Submit", self)
+        self.button.clicked.connect(self.onsubmit)
+        form_layout.addRow(self.button)
+
+        self.outputlabel = QtWidgets.QLabel("", self)
+        form_layout.addRow(self.outputlabel)
         #Custom Code End
         self.show()
+
+    def onsubmit(self, _):
+        name = self.line1.text()
+        print(name)
+        self.outputlabel.setText(f"Hello, {name}")
+
 
 #only executes if thsi file is explicitly called
 if __name__ == "__main__":
